9480b972a1964ebd3e74e6559b2387ad2825c96e
victor732131 <victorelbombillero@gmail.com>
2024-09-14 17:15:08 +0200

    ejercicio02

---
+++ tema01/Ejercicio01.java
@@ -0,0 +1,7 @@
+package tema01;
+
+public class Ejercicio01 { // Clase principal
+    public static void main(String[] args) {
+        System.out.println("victor");
+    }
+}
---
+++ tema01/Ejercicio02.java
@@ -0,0 +1,15 @@
+package tema01;
+
+/**
+ * 
+ * Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas
+ */
+public class Ejercicio02 { // Clase principal
+    public static void main(String[] args) {
+        System.out.println("victor");
+        System.out.println("calle larios");
+        System.out.println("632 232 057");
+    }
+}
