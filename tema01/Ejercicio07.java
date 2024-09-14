f297b349afb0afc9ec9c9e00ca365a98decf3385
victor732131 <victorelbombillero@gmail.com>
2024-09-14 18:14:44 +0200

    ejercicios 7 y 8

---
+++ tema01/Ejercicio07.java
@@ -0,0 +1,16 @@
+package tema01;
+
+/**
+ * Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe
+ver únicamente el contorno hecho con asteriscos).
+
+ */
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        System.out.println("    *"); 
+        System.out.println("   * *");
+        System.out.println("  *   *  ");
+        System.out.println(" *     *");
+        System.out.println("*********");
+    }
+}
