5958763a5479855b708a1d4c39c136d44e22db0b
victor732131 <victorelbombillero@gmail.com>
2024-09-14 17:39:28 +0200

    ejercicio06

---
+++ tema01/Ejercicio06.java
@@ -0,0 +1,14 @@
+package tema01;
+/**
+ * Escribe un programa que pinte por pantalla una pirámide rellena a base de
+asteriscos. La base de la pirámide debe estar formada por 9 asteriscos
+ */
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        System.out.println("    *"); 
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }   
+}
